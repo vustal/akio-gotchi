9c53b5b0ff08cdc80239ea03d69dd370e49e835b
vustal <vita.ustalkova@bk.ru>
2022-12-10 21:03:16 +0300

    add: project and gifs

---
+++ Akiogochy/Akiogochy/AkiogochyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AkiogochyApp.swift
+//  Akiogochy
+//
+//  Created by Vita on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct AkiogochyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Akiogochy/Akiogochy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Akiogochy
+//
+//  Created by Vita on 10.12.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
