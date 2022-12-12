e4cea1fe53141e29bbb1095b4cc2d3650c3d309c
vustal <vita.ustalkova@bk.ru>
2022-12-12 13:43:13 +0300

    add: func player for img and intro view

--- Akiogochy/Akiogochy/ContentView.swift
+++ Akiogochy/Akiogochy/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var click = 0
+    
+    func imgPlayer() {
+        var i = 0
+        Timer.scheduledTimer(withTimeInterval: 0.04, repeats: true) { timer in
+            i += 1
+            click += 1
+            if click == 69 {
+                timer.invalidate()
+                click = 0
+            }
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image("\(click)")
+                .resizable()
+                .scaledToFit()
+            Text("Hmmm...")
+                .padding()
+            Button("CLICK") {
+              imgPlayer()
+            }
+        }
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        VStack{
+            ContentView()
+        }
     }
 }
